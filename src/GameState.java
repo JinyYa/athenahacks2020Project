59a267df4bf1295b1c26674aeb27586437856165
Navina Mahesh <navina@Navinas-MacBook-Air.local>
2020-03-07 22:03:12 -0800

    l

--- src/GameState.java
+++ src/GameState.java
@@ -2,6 +2,15 @@
 public class GameState {
     public static final int MAX_RESOURCE = 20;
     public static final int NO_ROOMMATE = 0;
+    public static final int HAPPY_ENDING = 0;
+    public static final int MONEY_SAD = 1;
+    public static final int STUDY_SAD = 2;
+    public static final int MENTAL_HEALTH_SAD = 3;
+    public static final int SOCIAL_SAD = 4;
+    public static final int CLUB_SAD = 5;
+
+
+
 
     private int money;
     private int studying;
@@ -10,10 +19,8 @@ public class GameState {
     //private int[10] flags;
     private int club;
 
-    //do we add this?
-    private int grade;
     private int roommate;
-
+    private int endingType;
 
     public GameState(){ //constructor method
         //resources
@@ -44,8 +51,8 @@ public class GameState {
     }
 
     //setters
-    public void changeGrade(int change){
-        grade += change;
+    public void changeStudying(int change){
+        studying += change;
     }
 
     public void updateGameState(int userChoice, Card card){
@@ -61,19 +68,24 @@ public class GameState {
         //studying 0
         if(money == 0){
             //set gameendType to certain int
+          endingType = MONEY_SAD;
           return true;
         }
 
         else if(studying == 0){
+          endingType = STUDY_SAD;
           return true;
         }
         else if(mental_health == 0){
+          endingType = MENTAL_HEALTH_SAD;
           return true;
         }
         else if(social ==0){
+          endingType = SOCIAL_SAD;
           return true;
         }
         else if (club == 0){
+          endingType = CLUB_SAD;
           return true;
         }
         else{
@@ -84,7 +96,26 @@ public class GameState {
     }
 
     public int getEndingType(){
-      return 0;
+      if(gameOver() == false){
+        return HAPPY_ENDING;
+      }
+      else if (endingType == MONEY_SAD){
+        return MONEY_SAD;
+      }
+      else if(endingType == STUDY_SAD){
+        return STUDY_SAD;
+      }
+      else if(endingType == MENTAL_HEALTH_SAD){
+        return MENTAL_HEALTH_SAD;
+      }
+      else if(endingType == SOCIAL_SAD){
+        return SOCIAL_SAD;
+      }
+      else if(endingType == CLUB_SAD){
+        return CLUB_SAD;
+      }
+      return -1;
+
     }
 
 
