f69d3058c8f3c16b0ecf7a4770e643eb309b25ce
Jiny <jinyyang@usc.edu>
2020-03-07 16:35:26 -0800

    update to card

--- src/Card.java
+++ src/Card.java
@@ -5,13 +5,19 @@ public class Card {
     private String Option_A_text;
     private String Option_B_text;
 
+    private Card* Option_A_next;
+    private Card* Option_B_next;
+
+    public void show_card(){
+
+    }
+
     public void option_A_execute(){
 
     }
+
     public void option_B_execute(){
 
     }
-
-
 
 }
